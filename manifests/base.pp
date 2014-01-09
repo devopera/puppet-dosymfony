60ea1dd1b89f0154e4f13c74201f081cfe2e77a1
cleverlight <github.account@cleveraspaint.com>
2014-01-09 21:38:51 +0000

    removed interaction from the puppetted symfony install

--- manifests/base.pp
+++ manifests/base.pp
@@ -17,7 +17,7 @@ class dosymfony::base (
   # install symfony project to web root and symlink from home
   exec { 'dosymfony-base-install-symfony' :
     path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
-    command => "bash -c \"export HOME=/home/${user}/; composer.phar create-project symfony/framework-standard-edition ${target_path}/symfony-demo; ln -s ${target_path}/symfony-demo /home/${user}/\"",
+    command => "bash -c \"export HOME=/home/${user}/; composer.phar --no-interaction create-project symfony/framework-standard-edition ${target_path}/symfony-demo; ln -s ${target_path}/symfony-demo /home/${user}/\"",
     user => $user,
     group => $group,
     require => [Class['dosymfony::composer']],
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,6 @@ class dosymfony (
   # ---------------
   # setup defaults
 
-  $user = 'web',
-
   # end of class arguments
   # ----------------------
   # begin class
@@ -15,5 +13,8 @@ class dosymfony (
   if ! defined(Class['dosymfony::composer']) {
     class { 'dosymfony::composer': }
   }
-  
+
+  # anchor dosymfony contained classes
+  # @see http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
+  anchor { 'dosymfony-comp-first': } -> Class['dosymfony::composer'] -> anchor { 'dosymfony-comp-last': }
 }
