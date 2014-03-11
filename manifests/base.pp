83814e18eff3c1f4221a1b47ad1f66cc9d20a83f
cleverlight <github.account@cleveraspaint.com>
2014-03-11 19:59:37 +0000

    working symfony base, needs vhosts etc

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,4 @@
-class dosymfony::base (
+define dosymfony::base (
 
   # class arguments
   # ---------------
@@ -7,6 +7,7 @@ class dosymfony::base (
   $user = 'web',
   $group = 'www-data',
   $target_path = '/var/www/html',
+  $symlinkdir = false,
 
   # end of class arguments
   # ----------------------
@@ -15,12 +16,24 @@ class dosymfony::base (
 ) {
 
   # install symfony project to web root and symlink from home
-  exec { 'dosymfony-base-install-symfony' :
+  exec { "dosymfony-base-install-${title}" :
     path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
-    command => "bash -c \"export HOME=/home/${user}/; composer.phar --no-interaction create-project symfony/framework-standard-edition ${target_path}/symfony-demo; ln -s ${target_path}/symfony-demo /home/${user}/\"",
+    command => "bash -c \"export HOME=/home/${user}/; composer.phar --no-interaction create-project symfony/framework-standard-edition ${target_path}/${title}\"",
     user => $user,
     group => $group,
+    cwd => "/home/${user}",
     require => [Class['dosymfony::composer']],
+    # don't re-install if symfony directory already present (checked by 'src' subfolder)
+    creates => "${target_path}/${title}/src",
+  }
+  
+  if ($symlinkdir) {
+    # create symlink from install to directory (e.g. user's home folder)
+    file { "${symlinkdir}/${title}":
+      ensure => 'link',
+      target => "${target_path}/${title}",
+      require => Exec["dosymfony-base-install-${title}"],
+    }
   }
   
 }
